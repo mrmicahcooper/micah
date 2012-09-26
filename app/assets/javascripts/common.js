7a1d66c1d6dc95e704b0724105dddebfde7db07c
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2012-09-26 00:29:35 -0400

    Change teh colorz :smile:

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,3 +1,12 @@
 $(function() {
   $.support.hrefNormalized && DummyImage.generate();
+
+  var colors = ['#2f98dd','#a0511d', '#229b2e', '#dcde37']
+
+  $('section a, section h1, section h2').each(function(){
+    $link = $(this)
+    new_color = colors[Math.floor(Math.random() * colors.length)]
+    $link.css('color', new_color);
+  });
+
 });
