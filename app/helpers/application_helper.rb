337eafe3a13aef077c83144f890b98d7c235b36c
Micah Cooper and Taylor Mock <dev+mrmicahcooper+tmock12@hashrocket.com>
2012-10-09 11:28:43 -0400

    Add active_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def active_link_to(text, path, options={})
+    link_to(text, path, options.merge({class: active?(path)}))
+  end
+
+  def active?(path)
+    "active" if /^#{path}/ =~ request.path
+  end
+
 end
